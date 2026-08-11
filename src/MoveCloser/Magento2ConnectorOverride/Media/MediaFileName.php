5a5adc304d2b48cd9382ab5d0a3575c4f11ae93e
Hubert Smusz <hubert.smusz@movecloser.pl>
2026-08-11 22:52:01 +0200

    normalizacja nazw zdjec: wielokrotny prefiks, sufiks kolizji 1-99, male litery i [-_] jednakowo

--- src/MoveCloser/Magento2ConnectorOverride/Media/MediaFileName.php
+++ src/MoveCloser/Magento2ConnectorOverride/Media/MediaFileName.php
@@ -23,16 +23,30 @@ final class MediaFileName
      */
     private const HASH_PREFIX = '/^(?=[0-9a-f]*[a-f])[0-9a-f]{8,40}_/';
 
-    private const COLLISION_SUFFIX = '/_\d+(\.[^.]+)$/';
+    /**
+     * Magento numeruje kolizje od 1 w górę, bez zer wiodących. Zawężenie do tej postaci chroni
+     * człony nazwy będące fragmentem kodu produktu, np. `..._01_02.png`.
+     */
+    private const COLLISION_SUFFIX = '/_[1-9][0-9]?(\.[^.]+)$/';
 
     /**
      * Nazwa sprowadzona do postaci porównywalnej między PIM a Magento.
      */
     public static function normalize(string $fileName): string
     {
-        $stripped = (string) preg_replace(self::HASH_PREFIX, '', $fileName, 1);
+        // Prefiksy potrafią się nawarstwiać: plik pobrany z Magento i wstawiony do magazynu PIM
+        // niesie identyfikator z Magento pod hashem PIM. Zdejmowane są wszystkie.
+        $stripped = $fileName;
 
-        return (string) preg_replace(self::COLLISION_SUFFIX, '$1', $stripped);
+        while (1 === preg_match(self::HASH_PREFIX, $stripped)) {
+            $stripped = (string) preg_replace(self::HASH_PREFIX, '', $stripped, 1);
+        }
+
+        $stripped = (string) preg_replace(self::COLLISION_SUFFIX, '$1', $stripped);
+
+        // Ta sama grafika bywa wgrywana raz z myślnikami, raz z podkreśleniami, raz inną wielkością
+        // liter - dla dopasowania to jedna nazwa.
+        return strtolower((string) preg_replace('/[-_]+/', '_', $stripped));
     }
 
     /**
